b3c0dd841be4aa49476c63a42d34d903997f0683
benjamintyzack <b7008109@my.shu.ac.uk>
2022-02-17 12:49:26 +0000

    Finished task 4, 5 and 6

--- Zumo_Program/Zumo_Program.ino
+++ Zumo_Program/Zumo_Program.ino
@@ -2,29 +2,34 @@
 #include <Zumo32U4LineSensors.h>
 #include <Zumo32U4ProximitySensors.h>
 #include <Zumo32U4Buzzer.h>
+#include <Zumo32U4Encoders.h>
 //Serial1 communicates over XBee
 //Serial communicates over USB cable
 Zumo32U4Motors motors;
 Zumo32U4LineSensors sensors;
 Zumo32U4Buzzer buzzer;
 Zumo32U4ProximitySensors proxSensors;
+Zumo32U4Encoders encoders;
 
 //Speed Declarations
 #define TURN_SPEED        150
-#define CALIBERATE_SPEED  200
-#define BACKWARD_SPEED    150
-#define FORWARD_SPEED     200
+#define CALIBERATE_SPEED  150
+#define BACKWARD_SPEED    100
+#define FORWARD_SPEED     100
 #define STOP_SPEED        0                             // Setting engine speed to zero
 
 //Storage and declarations for line sensor used within course
-#define QTR_THRESHOLD     750               // microseconds
 #define NUM_SENSORS       3                 //declaring sensor count
 unsigned int lineSensorValues[NUM_SENSORS]; //array for line sensor values
 
 int roomNumber = 0;
 int endCounter = 0;
+int calibrateData[3];
+int encodersCountLeft;
+int encodersCountRight;
 String foundRooms[50];
 String roomDirection;
+boolean enableMessages = true;
 int robotStatus;
 int incomingByte; // for incoming serial data
 void setup() {
@@ -46,15 +51,17 @@ void loop() {
       break;
     case 1:
       autonomous();
-    case 2:
-      reachedImpass();
+      break;
   }
 }
 
 void calibrateRobot() {
 
   sensors.initThreeSensors();
+  delay(100);
   proxSensors.initFrontSensor();
+  delay(100);
+  encoders.init();
     // Wait 1 second and then begin automatic sensor calibration
   // by rotating in place to sweep the sensors over the line
   delay(1000);
@@ -70,6 +77,10 @@ void calibrateRobot() {
     }
 
     sensors.calibrate();
+  }
+
+  for(int i= 0; i < NUM_SENSORS; i++) {
+    calibrateData[i] = sensors.calibratedMaximumOn[i];
   }
   motors.setSpeeds(STOP_SPEED, STOP_SPEED);
   buzzer.play(">g32>>c32");
@@ -105,15 +116,11 @@ void manualControl() {
     motors.setSpeeds(STOP_SPEED, STOP_SPEED);
   }
   else if(incomingByte == 'q') {
-    Serial1.println("Rotating Left");
-    motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
-    delay(500);
+    turnLeft90();
     motors.setSpeeds(STOP_SPEED, STOP_SPEED);
   }
   else if(incomingByte == 'e') {
-    Serial1.println("Rotating Right");
-    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-    delay(500);
+    turnRight90();
     motors.setSpeeds(STOP_SPEED, STOP_SPEED);
   }
   else if(incomingByte == 'b') {
@@ -130,8 +137,7 @@ void manualControl() {
     // Save the room number and the side of it if user presses a is left but in other situations is right
     if (incomingByte == 'a')
     {
-      motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
-      delay(200);
+      turnLeft90();
       motors.setSpeeds(STOP_SPEED, STOP_SPEED);
       roomNumber++;
       foundRooms[roomNumber] = "left";
@@ -139,13 +145,15 @@ void manualControl() {
 
     else
     {
-      motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-      delay(200);
+      turnRight90();
       motors.setSpeeds(STOP_SPEED, STOP_SPEED);
       roomNumber++;
       foundRooms[roomNumber] = "right";
     }
-    Serial1.println("Room found is on the " + foundRooms[roomNumber]);
+    Serial1.print("Room ");
+    Serial1.print(roomNumber);
+    Serial1.print(" is on the ");
+    Serial1.print(foundRooms[roomNumber]);
     Serial1.println("Press C to search room");
 
     while (incomingByte != 'c')
@@ -171,9 +179,8 @@ void manualControl() {
       Serial1.println("Reached end " + endCounter);
       Serial1.print(" turning around");
       motors.setSpeeds(-BACKWARD_SPEED, -BACKWARD_SPEED);
-      delay(200);
-      motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-      delay(2000);
+      turnLeft90();
+      turnLeft90();
       motors.setSpeeds(STOP_SPEED, STOP_SPEED);
     }
     else if (endCounter == 2) {
@@ -192,79 +199,39 @@ void manualControl() {
 void autonomous() {
   incomingByte = Serial1.read();
 
-    sensors.read(lineSensorValues);
-    if(incomingByte == 'b') {
-      Serial1.println("Stopping for Room");
-      Serial1.println("Indicate which direction the room is, press A for left or D for right");
-      motors.setSpeeds(STOP_SPEED, STOP_SPEED);
-    
-      while ((incomingByte != 'a') && (incomingByte != 'd'))
-      {
-        incomingByte = (char) Serial1.read();
-      }
-
-      // Save the room number and the side of it if user presses a is left but in other situations is right
-      if (incomingByte == 'a')
-      {
-        motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
-        delay(200);
-        motors.setSpeeds(STOP_SPEED, STOP_SPEED);
-        roomNumber++;
-        foundRooms[roomNumber] = "left";
-      }
-
-      else
-      {
-        motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-        delay(200);
-        motors.setSpeeds(STOP_SPEED, STOP_SPEED);
-        roomNumber++;
-        foundRooms[roomNumber] = "right";
-      }
-      Serial1.println("Room found is on the " + foundRooms[roomNumber]);
-      Serial1.println("Press C to search room");
-
-      while (incomingByte != 'c')
-      {
-        incomingByte = (char) Serial1.read();
-      }
-
-      if(incomingByte == 'c') {
-        searchRoom();
-      }
-    }
-    //if left and right sensor get a black border reading then...
-    else if(((lineSensorValues[2] > QTR_THRESHOLD) && (lineSensorValues[0] > QTR_THRESHOLD)) || (lineSensorValues[1] > QTR_THRESHOLD)){ //if center sensor detects black line |sensor 1
-      //reached impass function
-      robotStatus = 2;
-      //delay half a second
-      delay(500);
-      motors.setSpeeds(0,0);
-      } 
-    //if left sesnor gets a black reading...
-    else if(lineSensorValues[0] > QTR_THRESHOLD){ //left sensor detects line | sensor 0
-      //go right for 1/4 second
-      motors.setSpeeds(TURN_SPEED, 0);
-      delay(250);
-      motors.setSpeeds(75, 75);
-    //if right sensor gets a black boarder reading...
-    }else if(lineSensorValues[2] > QTR_THRESHOLD){ //if right sensor detects black line| sensor 2
-      //go ;left for 1/4 second
-      motors.setSpeeds(0,  TURN_SPEED);
-      delay(250);
-      motors.setSpeeds(75, 75);
-    }
-   else if(incomingByte == 'k') {
-      motors.setSpeeds(STOP_SPEED, STOP_SPEED);
-      Serial1.println("EMERGENCY STOP!");
-      robotStatus = 0;
-      Serial1.println("Robot returned to manual mode");
-   }
-    else {
-      motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
-    }
-  
-
+  sensors.read(lineSensorValues);
+  if(incomingByte == 'b' && enableMessages == true) {
+    logRoom();
+    searchRoom();
+  }
+  //if left and right sensor get a black border reading then...
+  else if(((lineSensorValues[2] > calibrateData[2] + 100) && (lineSensorValues[0] > calibrateData[0] + 100) || (lineSensorValues[1] > calibrateData[1]))){ //if center sensor detects black line |sensor 1
+    endCounter++;
+    motors.setSpeeds(0,0);
+    junction();
+  } 
+  //if left sesnor gets a black reading...
+  else if(lineSensorValues[0] > calibrateData[0] + 100){ //left sensor detects line | sensor 0
+    //go right for 1/4 second
+    motors.setSpeeds(TURN_SPEED, 0);
+  //if right sensor gets a black boarder reading...
+  }else if(lineSensorValues[2] > calibrateData[2] + 100){ //if right sensor detects black line| sensor 2
+    //go ;left for 1/4 second
+    motors.setSpeeds(0,  TURN_SPEED);
+  }
+  else if (incomingByte == 'p') {
+    enableMessages = true;
+    Serial1.println("T Junctions passed, Room search enabled");
+  }
+  else if(incomingByte == 'k') {
+    motors.setSpeeds(STOP_SPEED, STOP_SPEED);
+    Serial1.println("EMERGENCY STOP!");
+    robotStatus = 0;
+    Serial1.println("Robot returned to manual mode");
+  }
+  else {
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
 }
 
 void searchRoom() {
@@ -272,9 +239,9 @@ void searchRoom() {
   delay(200);
   motors.setSpeeds(STOP_SPEED, STOP_SPEED);
 
-  for(uint16_t i = 0; i < 120; i++)
+  for(uint16_t i = 0; i < 60; i++)
   {
-    if (i > 30 && i <= 90)
+    if (i > 15 && i <= 45)
     {
       motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
       proxSensors.read();
@@ -286,99 +253,109 @@ void searchRoom() {
     }
   }
   motors.setSpeeds(STOP_SPEED, STOP_SPEED);
-  if(proxSensors.countsLeftWithLeftLeds() > 0 || proxSensors.countsFrontWithLeftLeds() > 0 || proxSensors.countsFrontWithRightLeds() > 0 || proxSensors.countsRightWithRightLeds() > 0) {
+  if(proxSensors.countsLeftWithLeftLeds() > 3 || proxSensors.countsFrontWithLeftLeds() > 3 || proxSensors.countsFrontWithRightLeds() > 3 || proxSensors.countsRightWithRightLeds() > 3) {
+    Serial1.println(" ");
     Serial1.println("Person found!");
+    buzzer.play(">g32>>c32");
   }
   else {
+    Serial1.println(" ");
     Serial1.println("No Person found");
   }
   motors.setSpeeds(-BACKWARD_SPEED, -BACKWARD_SPEED);
   delay(200);
+  if(foundRooms[roomNumber] == "left") {
+    turnRight90();
+  }
+  else {
+    turnLeft90();
+  }
   motors.setSpeeds(STOP_SPEED, STOP_SPEED);
+}
 
-  Serial1.println("If action is complete, press C to continue");
-  while (incomingByte != 'c')
+void turnRight90() {
+  encodersCountRight = 0;
+  encodersCountLeft = 0;
+  encoders.getCountsAndResetRight();
+  encoders.getCountsAndResetLeft();
+  motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+  while ((encodersCountRight > -700)) {
+    encodersCountRight = encoders.getCountsRight();
+  }
+  encodersCountRight = encoders.getCountsAndResetRight();
+  encodersCountLeft = encoders.getCountsAndResetLeft();
+}
+
+void turnLeft90() {
+  encodersCountLeft = 0;
+  encodersCountRight = 0;
+  encoders.getCountsAndResetRight();
+  encoders.getCountsAndResetLeft();
+  motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+  while ((encodersCountLeft > -700)) {
+    encodersCountLeft = encoders.getCountsLeft();
+  }
+  encodersCountLeft = encoders.getCountsAndResetLeft();
+  encodersCountRight = encoders.getCountsAndResetRight();
+}
+
+void logRoom() {
+  incomingByte = Serial1.read();
+  Serial1.println("Stopping for Room");
+  Serial1.println("Indicate which direction the room is, press A for left or D for right");
+  motors.setSpeeds(STOP_SPEED, STOP_SPEED);
+    
+  while ((incomingByte != 'a') && (incomingByte != 'd'))
   {
     incomingByte = (char) Serial1.read();
   }
-}
 
-//when zumo reaches an impass
-void reachedImpass(){
-  incomingByte = Serial1.read();
-  motors.setSpeeds(0, 0); 
-  delay(250);
-  motors.setSpeeds(-BACKWARD_SPEED, -BACKWARD_SPEED);
-  delay(500);
-  motors.setSpeeds(0, 0);
-  delay(250);
-  Serial1.println("Reached Impass");
-  Serial1.println("Press Y for corner, T for T-Junction or L for end of hallway");
-  switch (incomingByte)
+  // Save the room number and the side of it if user presses a is left but in other situations is right
+  if (incomingByte == 'a')
   {
-  case 'y':
-    Serial1.println("Press A to turn left or D to turn right");
-    while ((incomingByte != 'a') && (incomingByte != 'd'))
-      {
-        incomingByte = (char) Serial1.read();
-      }
-
-      if (incomingByte == 'a')
-      {
-        motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
-        delay(500);
-        motors.setSpeeds(STOP_SPEED, STOP_SPEED);
-        robotStatus = 1;
-      }
-
-      else
-      {
-        motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-        delay(500);
-        motors.setSpeeds(STOP_SPEED, STOP_SPEED);
-        robotStatus = 1;
-      }
-    break;
-  case 't':
-    Serial1.println("T Junction reached, pressed A to turn left or D to turn right");
-    while ((incomingByte != 'a') && (incomingByte != 'd'))
-      {
-        incomingByte = (char) Serial1.read();
-      }
-
-      if (incomingByte == 'a')
-      {
-        motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
-        delay(500);
-        motors.setSpeeds(STOP_SPEED, STOP_SPEED);
-        robotStatus = 1;
-      }
-
-      else
-      {
-        motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-        delay(500);
-        motors.setSpeeds(STOP_SPEED, STOP_SPEED);
-        robotStatus = 1;
-      }
-    break;
-  case 'l':
+    turnLeft90();
     motors.setSpeeds(STOP_SPEED, STOP_SPEED);
-    endCounter++;
-    if(endCounter == 1) {
-      Serial1.println("Reached end " + endCounter);
-      Serial1.print(" turning around");
-      motors.setSpeeds(-BACKWARD_SPEED, -BACKWARD_SPEED);
-      delay(200);
-      motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-      delay(2000);
-      motors.setSpeeds(STOP_SPEED, STOP_SPEED);
-    }
-    else if (endCounter == 2) {
-      Serial1.println("Reached end " + endCounter);
-      Serial1.print(" end of corridor");
-    }
-    break;
+    roomNumber++;
+    foundRooms[roomNumber] = "left";
   }
 
+  else
+  {
+    turnRight90();
+    motors.setSpeeds(STOP_SPEED, STOP_SPEED);
+    roomNumber++;
+    foundRooms[roomNumber] = "right";
+  }
+
+
+  Serial1.print("Room ");
+  Serial1.print(roomNumber);
+  Serial1.print(" is on the ");
+  Serial1.print(foundRooms[roomNumber]);
+}
+
+void junction() {
+  incomingByte = Serial1.read();
+
+  if (incomingByte == 'a') {
+      Serial1.println("Turning Left");
+      turnLeft90();
+      robotStatus = 1;
+    }
+    else if (incomingByte == 'd') {
+      Serial1.println("Turning Right");
+      turnRight90();
+      robotStatus = 1;
+    }
+    else if (incomingByte == 'u') {
+      turnLeft90();
+      turnLeft90();
+      if (endCounter == 3) {
+        enableMessages = false;
+      }
+      robotStatus = 1;
+    }
+    else if (endCounter == 4) {
+      Serial1.println("End of hallway reached");
+    }
 }
